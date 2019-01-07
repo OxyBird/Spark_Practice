31605d74cb4ee90b9c72510926be3e3b07b17a9b
Oxana Orlova <45640718+OxyBird@users.noreply.github.com>
2019-01-07 14:54:55 -0600

    Add files via upload

---
+++ AirportsByLatitudeProblem.java
@@ -0,0 +1,48 @@
+package com.sparkTutorial.rdd.airports;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+
+import com.sparkTutorial.rdd.commons.Utils;
+
+public class AirportsByLatitudeProblem {
+
+    public static void main(String[] args) throws Exception {
+
+        /* Create a Spark program to read the airport data from in/airports.text,  find all the airports 
+         * whose latitude are bigger than 40.
+           Then output the airport's name and the airport's latitude to out/airports_by_latitude.text.
+
+           Each row of the input file contains the following columns:
+           Airport ID, Name of airport, Main city served by airport, Country where airport 
+           is located, IATA/FAA code,
+           ICAO Code, Latitude, Longitude, Altitude, Timezone, DST, Timezone in Olson format
+
+           Sample output:
+           "St Anthony", 51.391944
+           "Tofino", 49.082222
+           ...
+         */
+    	SparkConf conf = new SparkConf().setAppName("AirPorts").setMaster("local[2]");
+		JavaSparkContext sc = new JavaSparkContext(conf);
+		
+		//find airports with latitude bigger than 40
+		JavaRDD<String>Airportgeneral = sc.textFile("in/airports.text");
+		/*first we filtered and than we use it new RDD (Airesultfilters)  as a base for maping*/
+		JavaRDD<String> Airesultfilters = Airportgeneral.filter(line -> Double.valueOf(line.split(Utils.COMMA_DELIMITER)[6]) > 40);
+		
+		JavaRDD<String>Airtable = Airesultfilters.map(line -> {
+		String[] split = line.split(Utils.COMMA_DELIMITER);
+        return StringUtils.join(new String[]{split[1], split[6]}, ",");
+    }
+);
+		        Airtable.saveAsTextFile("out/airports_by_latitude.text");
+		    }
+		}
+
+		
+		
+		
+		
